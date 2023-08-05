11b6bf4865f828fcfa2cb1e6d84c126ac7967f7a
Your Name <you@example.com>
2023-08-05 12:39:56 +0530

    first commit

---
+++ ps0_problem1.py
@@ -0,0 +1,20 @@
+#import module math to access the log function
+
+import math
+
+#Asks the user to enter a number x
+
+x = (int(input("Enter the number:")))
+
+#Asks the user to enter a number y
+
+y = (int(input("Enter the number:")))
+
+#Prints out number x, raised to the power y
+
+print("x**y =", x**y)
+
+#Prints out the log (base 2) of “x”.
+
+print("log(x) =", math.log(x, 2))
+
